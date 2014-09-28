6ee3c57696f7083e0d3605354e79aaae43eec5f1
Matthew Kevins <mmkevins@yahoo.com>
2014-09-28 14:09:07 -0400

    pass phase IVb; finish phase IV; finish project

--- skeleton/lib/04_associatable2.rb
+++ skeleton/lib/04_associatable2.rb
@@ -5,6 +5,27 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.model_class.table_name
+      source_table = source_options.model_class.table_name
+      foreign_key_value = self.send(through_options.foreign_key)
+
+
+      source_options.model_class.parse_all(DBConnection.execute(<<-SQL, foreign_key_value)).first
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+        ON
+          #{through_table}.#{source_options.foreign_key} = #{source_table}.#{source_options.primary_key}
+        WHERE
+          #{through_table}.#{through_options.primary_key} = ?
+      SQL
+    end
   end
 end
